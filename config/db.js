fd93ada1ebe229f041ad507b53d9758983b7b1ff
vinay <vinaykalva712@gmail.com>
2025-09-14 16:49:52 +0530

    added ingore files

--- config/db.js
+++ config/db.js
@@ -5,17 +5,22 @@ const mongoose = require('mongoose');
  * Establishes a connection to the MongoDB database using Mongoose.
  */
 const connectDB = async () => {
+  if (!process.env.MONGO_URI) {
+    console.error("❌ MONGO_URI is not defined in environment variables");
+    process.exit(1);
+  }
+
   try {
     const connection = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-
     console.log(`✅ MongoDB connected: ${connection.connection.host}`);
   } catch (error) {
     console.error(`❌ MongoDB connection failed: ${error.message}`);
     process.exit(1);
   }
 };
+
 
 module.exports = connectDB;
